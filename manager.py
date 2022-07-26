cc13f0a5b57ce9df3522805be7a6741b395e807e
Lsnow987 <75771744+Lsnow987@users.noreply.github.com>
2022-07-26 10:27:05 +0300

    cleaned up code

--- manager.py
+++ manager.py
@@ -1,3 +1,6 @@
+from time import time
+from turtle import distance
+from xmlrpc.client import DateTime
 import pandas as pd
 import os
 import numpy as np
@@ -5,6 +8,9 @@ import math
 from transformers import BertModel, BertTokenizerFast
 import torch
 from tqdm import tqdm
+import time
+import fpdf
+from unidecode import unidecode
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -17,7 +23,7 @@ class manager:
         self.paragraphs_df = pd.DataFrame(columns=['paragraph_id', 'paragraph_text', 'document_id','length']).astype('object')
 
     def loadCSV(self, filePath):
-        self.paragraphs_df = pd.read_csv(filePath, index_col=0, encoding_errors='ignore')
+        self.paragraphs_df = pd.read_csv(filePath, index_col=0) #encoding_errors='ignore'
 
     def loadDataFrame(self, filePath):
         self.paragraphs_df = pd.read_pickle(filePath)
@@ -83,6 +89,117 @@ class manager:
         print(self.getParagraph(paragraphID))
         return self.findClosest(self.getEmbeding(paragraphID,model),paragraphID, model, count)
 
+    def createPDF(self, distances, filename):
+
+        pdf = fpdf.FPDF()
+        # pdf.set_font("Arial", size=12)
+        pdf.add_font('DejaVu', '', '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansCondensed.ttf', uni=True)
+        pdf.set_font('DejaVu', '', 14)
+        # print(distances)
+        for row in distances.index:
+            # print(type(row))
+            pdf.add_page()
+        
+            pdf.cell(200, 10, txt=f"Paragraph ID: {row}", ln=1, align="C")
+            pdf.ln()
+            pdf.cell(200, 10, txt=f"Paragraph Distance: {distances[row]}", ln=1, align="C")
+            pdf.ln()            
+            getParagraph = self.getParagraph(row)
+            pdf.cell(200, 10, txt=f"Doc ID: {getParagraph['document_id']}", ln=1, align="C")
+            pdf.ln()            
+            text = self.getParagraph(row)[1].encode('cp1255',errors='replace').decode('cp1255',errors='replace')
+            text = text[::-1]
+            text = text.split()
+            text = np.flip(np.array(text))
+            line_index = 0
+            new_line = ""
+            for word in text:
+                if(line_index < 70):
+                    new_line = new_line + word + " "
+                    line_index = line_index + len(word) + 1
+                else:
+                    new_line = new_line.split()
+                    new_line = np.flip(np.array(new_line))
+                    new_line = " ".join(new_line)
+                    pdf.cell(0, 10, txt=new_line, align="R")
+                    pdf.ln()
+                    new_line = ""
+                    line_index = 0
+
+        pdf.cell(0, 10, txt=new_line, align="R")
+        pdf.ln()
+        pdf.output(filename, "F")
+        print("PDF Created")
+        return "distances.pdf"
+
+    def combine_files(self, directory):
+        #  Now Let's Read the Documents
+        # "/zooper2/jacob.khalili/docEmbeddings/Data/"
+        fileNames = os.listdir(directory)
+
+        count = 1
+        fileNames.sort()
+        for fileName in fileNames:
+            f = open(directory + fileName, "rb")
+            text = f.read()
+            full_text = text.decode("cp1255", errors="ignore")
+
+            if(count % 100 == 0):
+                print("Processing Document: " + str(count))
+            count += 1
+
+            # length_of_doc = len(full_text)
+            text_array = full_text.split("@")
+            paragraph_count = 1
+            # title = ""
+            # paragraph_list = list()
+            for paragraph in text_array:
+                if paragraph_count == 1:
+                    # title = paragraph.split("\r\n", 1)[:1]
+                    paragraph = paragraph.split("\r\n", 1)[1:]
+                else:
+                    paragraph = paragraph.split("\r\n", 1)[:]
+
+                if len(paragraph[0]) > 150:  # what number should this be to take out small paragraphs that don't mean anything
+                    theRebbe.addParagraph(paragraph[0], fileName,"discard")
+                paragraph_count += 1
+
+
+    def generate_embeddings(self, model, startValue, end):
+        alephbert_tokenizer = None
+        alephbert = None
+        if model == "1AlphaBert":
+            alephbert_tokenizer = BertTokenizerFast.from_pretrained('onlplab/alephbert-base')
+            alephbert = BertModel.from_pretrained('onlplab/alephbert-base')
+        else:
+            alephbert_tokenizer = BertTokenizerFast.from_pretrained("ysnow9876/alephbert-base-finetuned-for-shut")
+            alephbert = BertModel.from_pretrained("ysnow9876/alephbert-base-finetuned-for-shut")
+        # alephbert.eval()
+        counter = 0
+        for id in theRebbe.getParagraphIDs():
+            if id >= startValue:
+                text = theRebbe.getParagraph(id)['paragraph_text']
+                input = alephbert_tokenizer(text, return_tensors="pt", truncation=True, max_length=512, padding="max_length")
+                output = alephbert(**input)
+                enncoding = output.last_hidden_state[0,0,:]
+
+
+                #TODO I'm not sure what to do because the embedding are not the same size
+                theRebbe.addParagraphEmbedings(model,{id:enncoding.detach().numpy()})
+
+                if(counter % 100 == 0):
+                    print(counter)
+
+                if(counter % 5000 == 0 and counter != startValue and counter != 0):
+                    # theRebbe.exportCSV("all_paragraphs_final"+model+".csv")
+                    theRebbe.exportDataFrame("all_paragraphs_final"+model+".pkt")
+                counter += 1
+                
+                if counter == end:
+                    theRebbe.exportCSV("all_paragraphs_final"+model+".csv")
+                    theRebbe.exportDataFrame("all_paragraphs_final"+model+".pkt")
+                    break
+
 
     def findClosest(self, embedding, paragraphID, model, count):
 
@@ -94,112 +211,63 @@ class manager:
         all_info = theRebbe.getParagraph(paragraphID)
         doc_id = all_info[2]
         searchMe = self.paragraphs_df
-        # self.paragraphs_df.where(
-        #         self.paragraphs_df['document_id'] != doc_id
-        #     )
+        searchMe = searchMe
 
         print("Calculating Distances")
         #Calculating the distance between the embedding and the other embeddings
         
-        for i in tqdm(range(searchMe.shape[0])):
-            if (type(self.getEmbeding(i,model)) == type([0,0,0])):
-                searchMe.drop(labels=i, axis=0)
-                continue 
+        batch_size = 1024
+        num_rows = searchMe.shape[0]
+        num_batches = 66 #math.ceil(num_rows / batch_size)
 
+        #Create a Series
+        df = pd.DataFrame(columns=[])
+        distances = pd.Series()
+        for i in tqdm(range(num_batches)):
+            
+            start = i * batch_size
+            end = min((i + 1) * batch_size, num_rows)
+            batch = searchMe.iloc[start:end]
+            batch_embeddings = batch[model].values
 
-            c = torch.dist(torch.tensor(embedding).cuda(), torch.tensor(searchMe.at[i, model]).cuda())
-            searchMe.at[i, 'distance'] = c.item()
+            batch_embeddings=np.vstack(batch_embeddings).astype(np.float64)
+            batch_embeddings = torch.from_numpy(batch_embeddings)
+            
+            batch_embeddings = batch_embeddings - torch.tensor(embedding)
+            batch_embeddings = torch.norm(batch_embeddings, p=2, dim=1)
+    
+            new = pd.Series(batch_embeddings.numpy())
 
-        print("Sorting")
-        #Sorting the results by distance
+            distances = pd.concat([distances, new], axis=0, ignore_index=True)
 
-        searchMe = searchMe.sort_values(by='distance')
+        distances = distances.sort_values()
         
         #Returning the top count results
-        return searchMe.iloc[:(count+1), :]
+        return distances.iloc[:(count+1)]
 
 
 # Create a Test Manager Class
 theRebbe = manager()
-theRebbe.loadDataFrame("all_paragraphs_11000_2Alephbert-FineTuned-A.pkt")
-#  Now Let's Read the Documents
-# fileNames = os.listdir("/zooper2/jacob.khalili/docEmbeddings/Data")
-# all_documents = list()
+model = "1AlphaBert"
+dataframe = "all_paragraphs_final1AlphaBert.pkt"
+directory = "home/..."
+theRebbe.loadDataFrame(dataframe)
+theRebbe.combine_files(directory)
+theRebbe.addModel(model,duplicates='discard')
 
-# count = 1
-# fileNames.sort()
-# for fileName in fileNames:
-#     f = open("/zooper2/jacob.khalili/docEmbeddings/Data/" + fileName, "rb")
-#     text = f.read()
-#     full_text = text.decode("cp1255", errors="ignore")
+start = 0
+end = 100_000
+theRebbe.generate_embeddings(model, start, end)
 
-#     if(count % 100 == 0):
-#         print("Processing Document: " + str(count))
-#     count += 1
-
-#     length_of_doc = len(full_text)
-#     text_array = full_text.split("@")
-#     paragraph_count = 1
-#     title = ""
-#     paragraph_list = list()
-#     for paragraph in text_array:
-#         if paragraph_count == 1:
-#             title = paragraph.split("\r\n", 1)[:1]
-#             paragraph = paragraph.split("\r\n", 1)[1:]
-#         else:
-#             paragraph = paragraph.split("\r\n", 1)[:]
-
-#         if len(paragraph[0]) > 150:  # what number should this be to take out small paragraphs that don't mean anything
-#             theRebbe.addParagraph(paragraph[0], fileName,"discard")
-#             #theRebbe.addParagraph(paragraph, fileName)
-#         paragraph_count += 1
-
-
-model = "2Alephbert-FineTuned-A"
-# theRebbe.addModel(model,duplicates='discard')
-
-# # print("Data Added")
-# # #theRebbe.addParagraphEmbedings('AlphaBert',{0:[3,3,3],
-# #                                                 1:[[4,5,6],[7,8,9]]})
-
-# alephbert_tokenizer = BertTokenizerFast.from_pretrained("ysnow9876/alephbert-base-finetuned-for-shut")
-# alephbert = BertModel.from_pretrained("ysnow9876/alephbert-base-finetuned-for-shut")
-# alephbert.eval()
-
-# counter = 0
-# print(theRebbe.paragraphs_df)
-# for id in theRebbe.getParagraphIDs():
-#     text = theRebbe.getParagraph(id)['paragraph_text']
-#     input = alephbert_tokenizer(text, return_tensors="pt", truncation=True, max_length=512, padding="max_length")
-#     output = alephbert(**input)
-#     enncoding = output.last_hidden_state
-
-
-#     #TODO I'm not sure what to do because the embedding are not the same size
-#     theRebbe.addParagraphEmbedings(model,{id:enncoding.detach().numpy()})
-
-#     if(counter % 100 == 0):
-#         print(counter)
-
-#     if(counter % 1000 == 0):
-#         theRebbe.exportDataFrame("all_paragraphs_" + str(counter) + "_" + str(model) +".pkt")
-#     counter += 1
+x = 0
+before = time.time()
+num_of_searches = 24
+while x < num_of_searches:
     
+    results1 = theRebbe.search(model,x,10)
+    theRebbe.createPDF(results1, f"all_paragraphs_final{model}_find10_"+str(x)+".pdf")
+    # results1.to_csv(str(x)+"_take_2_all_paragraphs_final"+model+"_find10.csv")
+    x+=1
 
-# theRebbe.exportCSV("all_paragraphs_final"+model+".csv")
-# theRebbe.exportDataFrame("all_paragraphs_final"+model+".pkt")
-# print(theRebbe.paragraphs_df)
-
-
-results = theRebbe.search(model,6,5)
-results.to_csv("all_paragraphs_final"+model+"_6_find5.csv")
-
-results1 = theRebbe.search(model,10,5)
-results1.to_csv("all_paragraphs_final"+model+"_10_find5.csv")
-
-results = theRebbe.search(model,76,5)
-results.to_csv("all_paragraphs_final"+model+"_76_find10.csv")
-
-print(theRebbe.getParagraph(6))
-print(theRebbe.getParagraph(10))
-print(theRebbe.getParagraph(76))
+after = time.time()
+print((after - before)/num_of_searches)
